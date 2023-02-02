5bb610b5da52d4d6952d38eebe3800445172cf33
JFonti <105557325+JFonti@users.noreply.github.com>
2023-02-02 12:47:51 -0500

    Create File Signing Interrogator.ps1
    A simple tool to check signing certificate Serial Number and Thumbprints.

---
+++ File Signing Interrogator.ps1
@@ -0,0 +1,19 @@
+﻿# Simple File Signature interrogation v.1
+# Joe Fonti  on  Aug 20, 2020
+
+
+$SignedFileTypes =@(".exe", ".dll", ".sys", ".inf", ".cat")
+$ToolDirectory = "C:\Program Files\Npcap\uninstall.exe"
+
+ForEach ($File in Get-ChildItem -Path "$ToolDirectory" -Recurse)
+{
+
+  if ($SignedFileTypes -contains $file.Extension)
+  {
+
+    Get-AuthenticodeSignature -FilePath $file.PSPath
+    Get-AuthenticodeSignature -FilePath $file.PSPath | Select-Object -ExpandProperty SignerCertificate | Select-object -Property SerialNumber, Thumbprint
+    
+
+  }
+} 
